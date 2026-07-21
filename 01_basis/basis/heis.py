226e2c35e994b62a189f8f8b66ff357b308dd7ee
Erik Torp <erik.torp@klp.no>
2026-07-21 10:53:46 +0200

    feature: heis fra 3.etg

--- 01_basis/basis/heis.py
+++ 01_basis/basis/heis.py
@@ -1,5 +1,6 @@
 # Enkelt program som tar inn en verdi og skriver ut etasjenummeret. 
 # Heisen går ikke til 13 etasjen. 
+# Spesielt for 3 etasje. 
 
 def etasje ():
 
@@ -8,8 +9,11 @@ def etasje ():
     if panel_etasje >= 13 :
         til_etasje = panel_etasje - 1
     else :
-        til_etasje = panel_etasje
-        
+        if panel_etasje == 3 :
+            til_etasje = 4
+        else :
+            til_etasje = panel_etasje
+
     # Skriver resultatet
     print("Heisen vil gå til: ", til_etasje)
     
